606e61bd1dc39066e3de97dd15d90abbfa1cadad
DSSon <ejrtmd2323@gmail.com>
2022-07-14 19:16:44 +0900

    Initial commit

---
+++ src/main/java/com/example/dsn2/Dsn2Application.java
@@ -0,0 +1,13 @@
+package com.example.dsn2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsn2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dsn2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dsn2/Dsn2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dsn2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsn2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
